db1e1b84067659c9d26a792aa9d9b02d5873f95c
Filipe Azevedo <pasnox@gmail.com>
2014-08-19 23:36:02 +0200

    Add support for os x to the runner.

--- runner/main.cpp
+++ runner/main.cpp
@@ -10,10 +10,14 @@ int main(int argc, char** argv) {
     Q_ASSERT(QFile::exists(uiFile));
 
     const QString qtTools = QLibraryInfo::location(QLibraryInfo::BinariesPath);
-    const QString designer = QDir(qtTools).entryInfoList(QStringList(QLatin1String("*designer*")), QDir::Files | QDir::Executable).value(0).absoluteFilePath();
+    const QString designer = QDir(qtTools).entryInfoList(QStringList(QLatin1String("*designer*")), QDir::Dirs | QDir::Files | QDir::Executable).value(0).absoluteFilePath();
 
+    QString appDirPath = app->applicationDirPath();
+#if defined(Q_OS_MACX)
+    appDirPath.append(QLatin1String("/../../.."));
+#endif
     QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
-    env.insert(QLatin1String("QT_PLUGIN_PATH"), app->applicationDirPath());
+    env.insert(QLatin1String("QT_PLUGIN_PATH"), appDirPath);
 
     QProcess* process = new QProcess(app.data());
     process->setProcessChannelMode(QProcess::ForwardedChannels);
