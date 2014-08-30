2b9ddce1fc7d689135b9adff79ff4fdeea4bc950
Nox P@sNox <pasnox@gmail.com>
2014-08-31 01:35:11 +0200

    Add cachedPixmapColor and cachedIconColor

--- qtwidgetsextra/QtWidgetsExtraCache.cpp
+++ qtwidgetsextra/QtWidgetsExtraCache.cpp
@@ -26,6 +26,24 @@ QPixmap QtWidgetsExtraCache::cachedPixmap(const QString &key)
     return pixmap;
 }
 
+QPixmap QtWidgetsExtraCache::cachedPixmapColor(const QColor &color, const QSize &size)
+{
+    const QString key = QString(QLatin1String("%1-%2x%3-pixmap"))
+        .arg(color.name(QColor::HexArgb))
+        .arg(size.width())
+        .arg(size.height())
+    ;
+    QPixmap pixmap = QtWidgetsExtraCache::cachedPixmap(key);
+
+    if (pixmap.isNull()) {
+        pixmap = QPixmap(size);
+        pixmap.fill(color);
+        QtWidgetsExtraCache::cachePixmap(key, pixmap);
+    }
+
+    return pixmap;
+}
+
 bool QtWidgetsExtraCache::cacheIcon(const QString &key, const QIcon &icon)
 {
     if (icon.isNull()) {
@@ -48,4 +66,25 @@ QIcon QtWidgetsExtraCache::cachedIcon(const QString &key)
 {
     QIcon* icon = cachedIcons.object(qHash(key));
     return icon ? *icon : QIcon();
+}
+
+QIcon QtWidgetsExtraCache::cachedIconColor(const QColor &color, const QSize &size)
+{
+    const QString key = QString(QLatin1String("%1-%2x%3-icon"))
+        .arg(color.name(QColor::HexArgb))
+        .arg(size.width())
+        .arg(size.height())
+    ;
+    QIcon icon = QtWidgetsExtraCache::cachedIcon(key);
+
+    if (icon.isNull()) {
+        QPixmap pixmap = QtWidgetsExtraCache::cachedPixmapColor(color, size);
+
+        if (!pixmap.isNull()) {
+            icon = QIcon(pixmap);
+            QtWidgetsExtraCache::cacheIcon(key, icon);
+        }
+    }
+
+    return icon;
 }
--- qtwidgetsextra/QtWidgetsExtraCache.h
+++ qtwidgetsextra/QtWidgetsExtraCache.h
@@ -5,13 +5,17 @@
 
 class QPixmap;
 class QIcon;
+class QColor;
+class QSize;
 
 namespace QtWidgetsExtraCache
 {
     bool cachePixmap(const QString &key, const QPixmap &pixmap);
     QPixmap cachedPixmap(const QString &key);
+    QPixmap cachedPixmapColor(const QColor &color, const QSize &size);
     bool cacheIcon(const QString &key, const QIcon &icon);
     QIcon cachedIcon(const QString &key);
+    QIcon cachedIconColor(const QColor &color, const QSize &size);
 }
 
 #endif // QTWIDGETSEXTRACACHE_H
