6dd5761fe7f45dd613994f4eaa8c8643cba8b8df
Filipe Azevedo <pasnox@gmail.com>
2014-09-14 13:25:24 +0200

    Add auto discovering of headers and plugin creation in body.

--- qtwidgetsextra/QtWidgetsExtra.cpp
+++ qtwidgetsextra/QtWidgetsExtra.cpp
@@ -1,31 +1,10 @@
 #include "QtWidgetsExtra.h"
-#include "QOpenFileButtonPlugin.h"
-#include "QOpenFileLineEditPlugin.h"
-#include "QSaveFileButtonPlugin.h"
-#include "QSaveFileLineEditPlugin.h"
-#include "QOpenFolderButtonPlugin.h"
-#include "QOpenFolderLineEditPlugin.h"
-#include "QColorLineEditPlugin.h"
-#include "QColorPushButtonPlugin.h"
-#include "QColorToolButtonPlugin.h"
-#include "QColorComboBoxPlugin.h"
+#include "generated/pluginsHeaders.h"
 
 QtWidgetsExtra::QtWidgetsExtra(QObject *parent)
     : QObject(parent)
 {
-    m_widgets.append(new QOpenFileButtonPlugin(this));
-    m_widgets.append(new QOpenFileLineEditPlugin(this));
-
-    m_widgets.append(new QSaveFileButtonPlugin(this));
-    m_widgets.append(new QSaveFileLineEditPlugin(this));
-
-    m_widgets.append(new QOpenFolderButtonPlugin(this));
-    m_widgets.append(new QOpenFolderLineEditPlugin(this));
-
-    m_widgets.append(new QColorLineEditPlugin(this));
-    m_widgets.append(new QColorPushButtonPlugin(this));
-    m_widgets.append(new QColorToolButtonPlugin(this));
-    m_widgets.append(new QColorComboBoxPlugin(this));
+    #include "generated/pluginsNew.h"
 }
 
 QList<QDesignerCustomWidgetInterface*> QtWidgetsExtra::customWidgets() const
